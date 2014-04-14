5ec4c4da87b10b4a47bdc826a9e1aa8a98c11ab7
Siddharth Agarwal <sid@less-broken.com>
2014-04-13 19:28:41 -0700

    version 0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.1.2'
+version = '0.1.3'
 
 long_description = (
     'Detailed Documentation\n'
