9e841ae071001ea099dc64a45409d952ba3d1c0e
if1live <libsora25@gmail.com>
2015-10-23 20:34:39 +0900

    * add function - is symbolic link, read link (for symbolic link) * symbolic link is not same with junction, but they share same data struct. so I modify reparse data buffer to reuse.

--- ntfsutils/symboliclink.py
+++ ntfsutils/symboliclink.py
@@ -8,24 +8,26 @@ __all__ = ["create"]
 
 import os
 from . import fs
+from . import junction
 from .fs import CreateSymbolicLink, GetLastError
 
 import ctypes
 from ctypes import WinError
 
 ERROR_PRIVILEGE_NOT_HELD = 1314
+ERROR_NOT_A_REPARSE_POINT = 4390
 
 class SymLinkPermissionError(Exception):
     pass
 
-def create(source, link_name):
+def create(source, link_name, ignore_validate = False):
     """
     Create a symbolic link at link_name pointing to source.
     Need administrator permission to create a symbolic link.
 
     http://stackoverflow.com/questions/6260149/os-symlink-support-in-windows
     """
-    if not os.path.exists(source):
+    if not os.path.exists(source) and not ignore_validate:
         raise Exception("%s: source is not exists." % source)
     if os.path.exists(link_name):
         raise Exception("%s: symbolic link name already exists" % link_name)
@@ -38,3 +40,41 @@ def create(source, link_name):
             raise SymLinkPermissionError("no permission, error code : %d" % error_code)
         else:
             raise WinError()
+
+def issymboliclink(path):
+    try:
+        reparseinfo = junction.readreparseinfo(path)
+    except OSError as e:
+        error_code = fs.GetLastError()
+        if error_code == ERROR_NOT_A_REPARSE_POINT:
+            return False
+        # default
+        return False
+
+    if reparseinfo.ReparseTag != junction.IO_REPARSE_TAG_SYMLINK:
+        return False
+
+    attrs = fs.GetFileAttributes(path)
+    return bool(attrs & fs.FILE_ATTRIBUTE_REPARSE_POINT)
+    
+def readlink(path):
+    if not issymboliclink(path):
+        raise Exception("%s does not exist or is not a symbolic link" % path)
+
+    reparseinfo = junction.readreparseinfo(path)
+    name_buffer = reparseinfo.readable_SubstituteNameBuffer
+
+    if name_buffer[0] == chr(1) and name_buffer[1] == chr(0):
+        # relative path
+        # example : \x01\x00dummy.txtdummy.txt
+        total_len = len(name_buffer)
+        return name_buffer[2:(total_len-2)//2 + 2]
+
+    elif name_buffer[0] == chr(0) and name_buffer[1] == chr(0):
+        # absoulte path
+        # example : \x00\x00c:\devel\ntfs\tests\data\dummy.txt\??\c:\devel\ntfs\tests\data\dummy.txt
+        total_len = len(name_buffer)
+        return name_buffer[2:(total_len-6)//2 + 2]
+
+    else:
+        raise Exception("unknown name buffer format : %s" % name_buffer)
--- tests/symboliclink_tests.py
+++ tests/symboliclink_tests.py
@@ -5,6 +5,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import unittest
 
 from ntfsutils import symboliclink
+from ntfsutils import junction
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -59,6 +60,127 @@ class create_for_file_Test(unittest.TestCase):
             print(e.message)
         except:
             self.fail()
+
+class issymboliclink_Test(unittest.TestCase):
+    def setUp(self):
+        self.source_file = os.path.join(BASE_PATH, 'data', 'dummy.txt')
+        self.source_dir = os.path.join(BASE_PATH, 'data')
+
+        self.link_name_file =  os.path.join(BASE_PATH, 'data', 'tmp-data.txt')
+        self.link_name_dir =  os.path.join(BASE_PATH, 'tmp-data')
+
+    def tearDown(self):
+        try:
+            os.removedirs(self.link_name_dir)
+        except OSError:
+            pass
+        try:
+            os.remove(self.link_name_file)
+        except OSError:
+            pass
+
+    def test_for_dir(self):
+        try:
+            symboliclink.create(self.source_dir, self.link_name_dir)
+        except symboliclink.SymLinkPermissionError as e:
+            return
+        except:
+            self.fail()    
+        self.assertTrue(symboliclink.issymboliclink(self.link_name_dir))
+
+    def test_for_file(self):
+        try:
+            symboliclink.create(self.source_file, self.link_name_file)
+        except symboliclink.SymLinkPermissionError as e:
+            return
+        except:
+            self.fail()    
+        self.assertTrue(symboliclink.issymboliclink(self.link_name_file))
+
+    def test_for_not_link_file(self):  
+        self.assertFalse(symboliclink.issymboliclink(self.source_file))
+    
+    def test_for_not_link_dir(self):
+        self.assertFalse(symboliclink.issymboliclink(self.source_dir))
+
+    def test_for_not_exist(self):
+        self.assertFalse(symboliclink.issymboliclink("invalid-path"))
+
+class readlink_for_file_Test(unittest.TestCase):
+    def setUp(self):
+        self.source = os.path.join(BASE_PATH, 'data', 'dummy.txt')
+        self.link_name =  os.path.join(BASE_PATH, 'data', 'tmp-data.txt')
+
+    def tearDown(self):
+        try:
+            os.remove(self.link_name)
+        except OSError:
+            pass
+
+    def test_absolute_path(self):
+        try:
+            symboliclink.create(self.source, self.link_name)
+            actual = symboliclink.readlink(self.link_name)
+            self.assertEqual(actual, self.source)
+        except symboliclink.SymLinkPermissionError as e:
+            print(e.message)
+        except:
+            self.fail()
+
+    def test_relative_path(self):
+        source = "dummy.txt"
+        try:
+            symboliclink.create(source, self.link_name, True)
+            actual = symboliclink.readlink(self.link_name)
+            self.assertEqual(actual, source)
+        except symboliclink.SymLinkPermissionError as e:
+            print(e.message)
+        except:
+            self.fail()
+
+class readlink_for_dir_Test(unittest.TestCase):
+    def setUp(self):
+        self.source = os.path.join(BASE_PATH, 'data')
+        self.link_name =  os.path.join(BASE_PATH, 'tmp-data')
+
+    def tearDown(self):
+        try:
+            os.removedirs(self.link_name)
+        except OSError:
+            pass
+        try:
+            os.remove(self.link_name)
+        except OSError:
+            pass
+
+    def test_absolute_path(self):
+        try:
+            symboliclink.create(self.source, self.link_name)
+            actual = symboliclink.readlink(self.link_name)
+            self.assertEqual(actual, self.source)
+        except symboliclink.SymLinkPermissionError as e:
+            print(e.message)
+        except:
+            self.fail()
+
+    def test_relative_path(self):
+        source = "tmp-data"
+        try:
+            symboliclink.create(source, self.link_name, True)
+            actual = symboliclink.readlink(self.link_name)
+            self.assertEqual(actual, source)
+        except symboliclink.SymLinkPermissionError as e:
+            print(e.message)
+        except:
+            self.fail()
+
+class readlink_for_not_symlink_Test(unittest.TestCase):
+    def setUp(self):
+        self.filename = os.path.join(BASE_PATH, 'data', 'dummy.txt')
+
+    def test_raise_exc(self):
+        with self.assertRaises(Exception) as cm:
+            symboliclink.readlink(self.filename)
 
 if __name__ == "__main__":
     unittest.main()
